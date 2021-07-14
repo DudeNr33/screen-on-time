459bcc52b2c8318e171904279c56a7eb08ece5f5
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-07-14 18:23:37 +0200

    Refactoring into subfunctions, formatting, linting

--- src/screen_on_time.py
+++ src/screen_on_time.py
@@ -11,35 +11,62 @@ import re
 import sys
 from datetime import datetime
 
-timestamp = r"(?P<timestamp>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s[+-]\d{4}"
-display_switched = re.compile(timestamp + "\s+\w+\s+Display is turned (?P<state>\w+)")
-charge_on_ac = re.compile(timestamp + "\s+\w+\s+.*Using AC \(Charge:(?P<charge>\d+)%\)")
+TIMESTAMP_REGEX = r"(?P<timestamp>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s[+-]\d{4}"
 
-timestamp_format = "%Y-%m-%d %H:%M:%S"
 
-raw_output = subprocess.check_output(["pmset", "-g", "log"])
-if sys.version_info.major >= 3:
-    pmset_log = str(raw_output, "utf-8")
-else:
-    pmset_log = raw_output
+def main():
+    pmset_log = get_pmset_log()
+    last_charge_timestamp, last_charge_value = get_last_charge_info(pmset_log)
+    display_on_duration = get_time_with_display_turned_on(pmset_log, starting_from=last_charge_timestamp)
+    hours = int(display_on_duration / 3600)
+    minutes = int(display_on_duration % 3600 / 60)
 
-last_charge_timestamp, last_charge_value = charge_on_ac.findall(pmset_log)[-1]
-all_times = display_switched.findall(pmset_log)
-display_times = [
-    status 
-    for status in all_times 
-    if datetime.strptime(status[0], timestamp_format) >= datetime.strptime(last_charge_timestamp, timestamp_format)
-]
-last_state_before_full_charge = all_times[-len(display_times)-1]
-durations = []
-last_on = datetime.strptime(last_charge_timestamp, timestamp_format)
-current_state = last_state_before_full_charge[-1]
-for entry in display_times:
-    if entry[-1] == "on" and current_state == "off":
-        last_on = datetime.strptime(entry[0], timestamp_format)
-    elif entry[-1] == "off" and current_state == "on":
-        durations.append((datetime.strptime(entry[0], timestamp_format) - last_on).total_seconds())
-    current_state = entry[-1]
-# current time
-durations.append((datetime.now() - last_on).total_seconds())
-print("Last charge was to {}%, since then actively used for {:.2f} hours".format(last_charge_value, sum(durations) / 60 / 60))
+    print("Last charge: {} to {}%".format(last_charge_timestamp, last_charge_value))
+    print("Usage duration with screen turned on: {}h {}min".format(hours, minutes))
+
+
+def get_pmset_log():
+    raw_output = subprocess.check_output(["pmset", "-g", "log"])
+    return str(raw_output, "utf-8") if sys.version_info.major >= 3 else raw_output
+
+
+def get_last_charge_info(pmset_log):
+    timestamp_raw, last_charge_value = re.findall(
+        TIMESTAMP_REGEX + r"\s+\w+\s+.*Using AC \(Charge:(?P<charge>\d+)%\)", pmset_log
+    )[-1]
+    return convert_timestamp(timestamp_raw), last_charge_value
+
+
+def get_time_with_display_turned_on(pmset_log, starting_from):
+    all_times = re.findall(
+        TIMESTAMP_REGEX + r"\s+\w+\s+Display is turned (?P<state>\w+)", pmset_log
+    )
+    display_times = [
+        status
+        for status in all_times
+        if convert_timestamp(status[0])
+        >= starting_from
+    ]
+    last_state_before_full_charge = all_times[-len(display_times) - 1]
+    durations = []
+    last_on = starting_from
+    current_state = last_state_before_full_charge[-1]
+    for entry in display_times:
+        if entry[-1] == "on" and current_state == "off":
+            last_on = convert_timestamp(entry[0])
+        elif entry[-1] == "off" and current_state == "on":
+            durations.append(
+                (convert_timestamp(entry[0]) - last_on).total_seconds()
+            )
+        current_state = entry[-1]
+    durations.append((datetime.now() - last_on).total_seconds())  # current usage
+    return sum(durations)
+
+
+def convert_timestamp(timestamp):
+    return datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
